21a4d18aac730deba288a3727308cd85cc6a3f75
Yuliya Borisenko <kazy1997@gmail.com>
2021-05-24 15:35:08 +0300

    added eslint rule of semi-colon to interface

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,32 +1,32 @@
 interface TaskType {
-    id: string,
-    name: string,
-    columnId: string,
-    createdAt: Date,
+    id: string;
+    name: string;
+    columnId: string;
+    createdAt: Date;
 }
 
 interface ColumnType {
-    id: string,
-    name: string,
-    tasks: Array<string>,
-    order: number,
+    id: string;
+    name: string;
+    tasks: Array<string>;
+    order: number;
 }
 
 interface ColumnStateType {
     byId: {
         [key: string]: ColumnType,
-    },
-    allIds: Array<string>,
+    };
+    allIds: Array<string>;
 }
 
 interface TaskStateType {
     byId: {
         [key: string]: TaskType,
-    },
-    allIds: Array<string>,
+    };
+    allIds: Array<string>;
 }
 
 interface StateType {
-    columns: ColumnStateType,
-    tasks: TaskStateType,
+    columns: ColumnStateType;
+    tasks: TaskStateType;
 }
